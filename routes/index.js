38494b95a3fabf90756c27be3083cbb225a9ee7c
Victor Akiti <14thchief@gmail.com>
2021-03-27 19:09:53 +0100

    Add a new route to display lead details

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var landing = require('../controllers/landing')
 /* GET home page. */
+router.get('/lead/:lead_id', landing.show_lead);
 router.get('/', landing.get_landing);
 router.get('/leads', landing.show_leads);
 router.post('/', landing.submit_lead);
