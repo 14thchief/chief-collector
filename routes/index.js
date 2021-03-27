450d7d6d4d42ac799e09aef64a48e88b68fe57f6
Victor Akiti <14thchief@gmail.com>
2021-03-27 19:43:39 +0100

    Add lead edit and update/post route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 var landing = require('../controllers/landing')
 /* GET home page. */
-router.get('/lead/:lead_id', landing.show_lead);
 router.get('/', landing.get_landing);
-router.get('/leads', landing.show_leads);
 router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads);
+router.get('/lead/:lead_id', landing.show_lead);
+router.get('/lead/:lead_id/edit', landing.show_edit_lead);
+router.post('/lead/:lead_id/edit', landing.edit_lead);
 module.exports = router;
