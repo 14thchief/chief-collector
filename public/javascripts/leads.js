2be3f34f7b163d9f579995ee58b8f3e09266781e
Victor Akiti <14thchief@gmail.com>
2021-04-01 13:54:56 +0100

    implement lead (client-side) delete function call using jquery ajax request

---
+++ public/javascripts/leads.js
@@ -0,0 +1,20 @@
+const { post } = require("../../app");
+
+
+function deleteLead(leadId)
+{
+    return $.ajax({
+        url: '/lead/' + leadId + '/delete-json',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: JSON.stringify({leadId}),
+        type: 'POST',
+        success: ((res)=> {
+            console.log("Result: ", res)
+            $("#"+leadId).remove();
+        }),
+        error: ((error) => {
+            console.log("Error:", error);
+        })
+    });
+}
