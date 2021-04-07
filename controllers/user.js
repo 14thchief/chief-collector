efe816c6f44e5c0f5eaaca436778621474fe5344
Victor Akiti <14thchief@gmail.com>
2021-04-08 00:38:09 +0100

    setup and implement passport js local auth strategy: using email and password fields

--- controllers/user.js
+++ controllers/user.js
@@ -8,4 +8,10 @@ module.exports.show_signup = function(req, res, next) {
    return res.render('user/signup', { formData: {}, error: {} });
 }
 
+module.exports.login = function(req, res, next) {
 
+}
+
+module.exports.signup = function(req, res, next) {
+   
+}
