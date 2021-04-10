6632ef91df5dd0a3f26427f378ce1145ca458d6e
Victor Akiti <14thchief@gmail.com>
2021-04-10 01:35:15 +0100

    Implemented validation and error handling for the signup route

--- controllers/user.js
+++ controllers/user.js
@@ -5,13 +5,25 @@ const myPassport = require('../passport_setup')(passport);
 let bcrypt = require('bcrypt');
 let flash = require('connect-flash');
 
+
+const {  validateUser } = require('../validators/signup');
+const { isEmpty } = require('lodash');
+
+
+
+
+
 module.exports.show_login = function(req, res, next) {
    return res.render('user/login', { formData: {}, errors: {} });
 }
 
 
 module.exports.show_signup = function(req, res, next) {
-   return res.render('user/signup', { formData: {}, error: {} });
+   return res.render('user/signup', { formData: {}, errors: {} });
+}
+
+const rerender_signup = function(errors, req, res, next) {
+   return res.render('user/signup', { formData: req.body, errors: errors });
 }
 
 
@@ -19,18 +31,29 @@ const generateHash = function(password) {
    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
+
+//signup handler  
 module.exports.signup = function(req, res, next) {
-   const newUser = models.User.build({
-      email: req.body.email,
-      password: generateHash(req.body.password)
-   });
-   return newUser.save().then(result => {
-      passport.authenticate('local', {
-         successRedirect: "/",
-         failureRedirect: "/signup",
-         failureFlash: true
-      })(req,res,next);
+   let errors = {};
+   return validateUser(errors, req).then(errors=> {
+      if(!isEmpty(errors)) {
+         rerender_signup(errors, req, res, next);
+      }
+      else{
+         const newUser = models.User.build({
+            email: req.body.email.toLowerCase(),
+            password: generateHash(req.body.password)
+         });
+         return newUser.save().then(result => {
+            passport.authenticate('local', {
+               successRedirect: "/",
+               failureRedirect: "/signup",
+               failureFlash: true
+            })(req,res,next);
+         })
+      }
    })
+   
 }
 
 
---
+++ validators/signup.js
@@ -0,0 +1,37 @@
+let models = require('../models')
+let validator = require('validator');
+
+const validateCreateUserFields = function(errors, req) {
+    if (req.body.email){
+        req.body.email = req.body.email.toLowerCase()
+    }
+    if  (!validator.isEmail(req.body.email)) {
+        errors["email"] = "Please enter a valid email.";
+    }
+    if (!validator.isAscii(req.body.password)) {
+        errors["password"] = "invalid characters in password, please try another one";
+    }
+    if (!validator.isLength(req.body.password, {min: 8, max: 25})) {
+        errors["password"] = "Please ensure your password has a minimum of 8 characters";
+    }
+}
+
+
+module.exports.validateUser = function(errors, req) {
+    return new Promise(function(resolve, reject){
+        validateCreateUserFields(errors, req);
+        return models.User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(u=> {
+            if (u !== null) { 
+                errors["email"] = "Email is already in use. Please Login or reset your password";
+            }
+            resolve(errors);
+        })
+    })
+   
+}
+
+
